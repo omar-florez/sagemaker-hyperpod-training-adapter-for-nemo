105aa694d00992c8b4c0f8269bfd1fe737502683
Omar Florez <oflorez@Omars-MacBook-Pro.local>
2025-11-12 20:17:48 -0500

    Fix: Acess use_sequence_packing from cfg.datadictionary instead of cfg.data

--- src/hyperpod_nemo_adapter/collections/data/hf_data_module.py
+++ src/hyperpod_nemo_adapter/collections/data/hf_data_module.py
@@ -42,7 +42,8 @@ class HuggingFaceDataModule(BaseDataModule):
     """
     def __init__(self, cfg: DictConfig, trainer: Trainer, collate_fn=None):
         # Check if sequence packing is enabled
-        self.use_packing = cfg.model.data.get("use_sequence_packing", False)
+        print(f"cfg:\n{cfg}")
+        self.use_packing = cfg.data.get("use_sequence_packing", False)
         self.tokenizer = None
         
         print("=" * 80)
